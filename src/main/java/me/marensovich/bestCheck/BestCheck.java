7f5a9669373496598f17dc5bdd1ce2475da26664
Andrey <98303133+marensovich@users.noreply.github.com>
2025-08-28 13:02:49 +0300

    Добавлена поддержка CommandAPI с затенением. Создана логика для файла конфигурации.

--- src/main/java/me/marensovich/bestCheck/BestCheck.java
+++ src/main/java/me/marensovich/bestCheck/BestCheck.java
@@ -1,16 +1,53 @@
 package me.marensovich.bestCheck;
 
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIBukkitConfig;
+import dev.jorel.commandapi.CommandAPICommand;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BestCheck extends JavaPlugin {
+
+public class BestCheck extends JavaPlugin {
+
+    private static BestCheck instance;
+    public static BestCheck getInstance() {
+        return instance;
+    }
+
+    private FileConfiguration config;
+    public FileConfiguration getConfig() {
+        return config;
+    }
 
     @Override
-    public void onEnable() {
-        getLogger().info("Plugin has been enabled!");
+    public void onLoad() {
+        CommandAPI.onLoad(new CommandAPIBukkitConfig(this).verboseOutput(true));
+        new CommandAPICommand("ping")
+                .executes((sender, args) -> {
+                    sender.sendMessage("pong!");
+                }).register();
+    }
+
+    @Override
+    public void reloadConfig() {
+        super.reloadConfig();
+        this.config = super.getConfig();
+    }
+
+    @Override
+    public void onEnable() {instance = this;
+        saveDefaultConfig();
+        reloadConfig();
+
+        CommandAPI.onEnable();
+
+        getLogger().info("BestCheck plugin enabled");
+
     }
 
     @Override
     public void onDisable() {
+        CommandAPI.onDisable();
         getLogger().info("Plugin has been disabled!");
     }
 }
